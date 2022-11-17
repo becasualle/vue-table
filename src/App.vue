8816d8c66db4735a90453b31ef7dafe88ca23779
Mark <mark.nelubin@gmail.com>
2022-11-17 21:21:02 +0700

    Better typing for columns data and rendering table rows with mock data

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,27 @@
   <header></header>
 
   <main>
-    <DataTable :columns="tableColumns" />
+    <DataTable :data="tableData" :columns="tableColumns" />
   </main>
 </template>
 
 <script setup lang="ts">
-import { onMounted, watchEffect, ref } from "vue";
+import { watchEffect, ref, computed } from "vue";
 import DataTable from "./components/DataTable.vue";
 import { apiData } from "./data/api";
 import type { Data } from "./data/api";
 
+export type TableColumns =
+  | "Аватар"
+  | "ФИО"
+  | "Пол"
+  | "Страна"
+  | "Дата рождения"
+  | "Email"
+  | "Телефон";
+
 // Get data for the table
-const tableData = ref<Data | null>(null);
+const tableInfo = ref<Data | null>(null);
 
 watchEffect(async () => {
   console.log("watch effect");
@@ -21,12 +30,12 @@ watchEffect(async () => {
   const data = await new Promise((resolve) => {
     setTimeout(() => {
       resolve(apiData);
-    }, 2000);
+    }, 500);
   });
-  tableData.value = data as Data;
+  tableInfo.value = data as Data;
 });
 
-const tableColumns = [
+const tableColumns: TableColumns[] = [
   "Аватар",
   "ФИО",
   "Пол",
@@ -35,6 +44,8 @@ const tableColumns = [
   "Email",
   "Телефон",
 ];
+
+const tableData = computed(() => tableInfo.value?.results);
 </script>
 
 <style scoped lang="scss"></style>
--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -1,21 +1,48 @@
 <template>
-  <table>
+  <table v-if="isFiltredData">
     <thead>
-      <th v-for="(name, index) in columns" :key="index">
-        {{ name }}
-        <span class="arrow asc"></span>
-      </th>
+      <tr>
+        <th v-for="(name, index) in columns" :key="index">
+          {{ name }}
+          <span class="arrow asc"></span>
+        </th>
+      </tr>
     </thead>
-    <tbody></tbody>
+    <tbody>
+      <tr v-for="entry in filtredData" :key="entry.login.uuid">
+        <td v-for="(column, index) in columns" :key="index">
+          {{ rowsData[column] }}
+        </td>
+      </tr>
+    </tbody>
   </table>
 </template>
 
 <script setup lang="ts">
+import { computed } from "vue";
+import type { Result } from "../data/api";
+import type { TableColumns } from "../App.vue";
 interface Props {
-  columns: string[];
+  data?: Result[];
+  columns: TableColumns[];
 }
 
 const props = defineProps<Props>();
+
+const filtredData = computed(() => props.data);
+const isFiltredData = computed(
+  () => filtredData.value && filtredData.value.length
+);
+
+const rowsData = computed(() => ({
+  Аватар: "https://randomuser.me/api/portraits/thumb/men/27.jpg",
+  ФИО: "Mr Noah Arnaud",
+  Пол: "male",
+  Страна: "France",
+  "Дата рождения": "1985-12-27T16:20:39.124Z",
+  Email: "noah.arnaud@example.com",
+  Телефон: "02-10-87-55-80",
+}));
 </script>
 
 <style lang="scss" scoped>
