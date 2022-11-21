f09c3477af36d601480444416bc6ed8ca9b294aa
Mark <mark.nelubin@gmail.com>
2022-11-21 21:57:06 +0700

    deploy config

---
+++ vue.config.js
@@ -0,0 +1,6 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "vue-table" : "/",
+};
