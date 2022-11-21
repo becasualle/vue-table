ccad39f6a09358b2ea78600591515697eb50c51c
Mark <mark.nelubin@gmail.com>
2022-11-21 22:30:01 +0700

    add vite base

--- vue.config.js
+++
@@ -1,6 +0,0 @@
-/* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
-
-module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "vue-table" : "/",
-};
